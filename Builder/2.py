1b97007d1dba7e2a29db0495f25f52a1c64c7ea1
belousov <tobigface@gmail.com>
2022-02-22 07:07:44 +0200

    DEL: extra files moved to a separate project.

--- Builder/2.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-tree_list = ['Parents', 'Children', 'GrandChildren']
-
-lambda d1: reduce(lambda x, y: {y: x}, tree_list[::-1], {})
-
-print(d1)
--- Builder/3.py
+++
@@ -1,7 +0,0 @@
-tree_list = ['Parents', 'Children', 'GrandChildren']
-
-tree_dict = {}
-for key in reversed(tree_list):
-    tree_dict = {key: tree_dict}
-
-print(tree_dict)
